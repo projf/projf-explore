4c688010d65c0bf265e78621d3c55d9912e67618
Will Green <WillGreen@users.noreply.github.com>
2021-01-05 18:20:51 +0000

    More logical argument order for verilator.

--- life-on-screen/lint.sh
+++ life-on-screen/lint.sh
@@ -9,13 +9,13 @@ DIR=`dirname $0`
 echo "## Linting top modules in ${DIR}/xc7"
 for f in ${DIR}/xc7/top_*.sv; do
     echo "##   Checking ${f}";
-    verilator -Wall --lint-only -I${DIR} -I${DIR}/xc7 \
+    verilator --lint-only -Wall -I${DIR} -I${DIR}/xc7 \
         -I${DIR}/../common -I${DIR}/../common/xc7 -I${DIR}/../common/xc7/null $f;
 done
 
 # echo "## Linting top modules in ${DIR}/ice40"
 # for f in ${DIR}/ice40/top_*.sv; do
 #     echo "##   Checking ${f}";
-#     verilator -Wall --lint-only -I${DIR} -I${DIR}/ice40 \
+#     verilator --lint-only -Wall -I${DIR} -I${DIR}/ice40 \
 #         -I${DIR}/../common -I${DIR}/../common/ice40 -I${DIR}/../common/ice40/null $f;
 # done
