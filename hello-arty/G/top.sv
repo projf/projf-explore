ff952c9ef06da3aee434363588a6c626725b285a
Will Green <WillGreen@users.noreply.github.com>
2021-02-12 11:01:44 +0000

    Make counter designs more consistent.

--- hello-arty/G/top.sv
+++ hello-arty/G/top.sv
@@ -1,5 +1,5 @@
 // Project F: Hello Arty G - Top
-// (C)2020 Will Green, open source hardware released under the MIT License
+// (C)2021 Will Green, open source hardware released under the MIT License
 // Learn more at https://projectf.io
 
 `default_nettype none
@@ -10,13 +10,18 @@ module top (
     output     logic [3:0] led
     );
 
-    localparam DIV_BY = 27'd100_000_000 - 27'd1;
+    localparam DIV_BY = 27'd100_000_000;
 
     logic stb;
     logic [26:0] cnt = 0;
     always_ff @(posedge clk) begin
-        cnt <= (cnt < DIV_BY) ? cnt + 1 : 0;
-        stb <= (cnt == DIV_BY) ? 1 : 0;
+        if (cnt != DIV_BY-1) begin
+            stb <= 0;
+            cnt <= cnt + 1;
+        end else begin
+            stb <= 1;
+            cnt <= 0;
+        end
     end
 
     always_ff @(posedge clk) begin
