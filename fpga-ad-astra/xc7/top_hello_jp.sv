c2f05e59cbae7c20e9d2a58dc1cee0d9c9c9dd5f
Will Green <WillGreen@users.noreply.github.com>
2021-04-06 16:31:41 +0100

    Tweak vector declaration..

--- fpga-ad-astra/xc7/top_hello_jp.sv
+++ fpga-ad-astra/xc7/top_hello_jp.sv
@@ -102,7 +102,7 @@ module top_hello_jp (
 
     // font ROM address
     logic [$clog2(FONT_HEIGHT)-1:0] spr_glyph_line [SPR_CNT];
-    logic spr_fdma [SPR_CNT];  // font ROM DMA slots
+    logic [SPR_CNT-1:0] spr_fdma;  // font ROM DMA slots
     always_comb begin
         font_rom_addr = 0;
         for (i = 0; i < SPR_CNT; i = i + 1) begin
