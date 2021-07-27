302ca9190208c1645b2002b67b8360fa14ba42f9
Will Green <WillGreen@users.noreply.github.com>
2021-07-27 17:16:42 +0100

    Improve spram nibble design.

--- lib/memory/ice40/spram_nibble.sv
+++ lib/memory/ice40/spram_nibble.sv
@@ -21,39 +21,46 @@ module spram_nibble #(
     );
 
 	logic we_p1;
-	logic [3:0] nibble, nibble_p1;
-	logic [15:0] dout_16, din_16;
+	logic  [3:0] nibble;
 	logic [13:0] addr_14;
+	logic  [1:0] addr_02, addr_02_p1;
+	logic [15:0] dout_16, din_16;
+
 
 	always_ff @(posedge clk) begin
-		case (nibble_p1)
-			4'b0001: data_out <= dout_16[3:0];
-			4'b0010: data_out <= dout_16[7:4];
-			4'b0100: data_out <= dout_16[11:8];
-			4'b1000: data_out <= dout_16[15:12];
-			default: data_out <= 4'b0000;  // should never occur
+		{addr_14,addr_02} <= addr;  // split the address
+		addr_02_p1 <= addr_02;      // delay one cycle for read output
+		we_p1 <= we;				// delay one cycle for address split
+	end
+
+	always_ff @(posedge clk) begin
+		case (addr_02_p1)
+			2'b00: data_out <= dout_16[3:0];
+			2'b01: data_out <= dout_16[7:4];
+			2'b10: data_out <= dout_16[11:8];
+			2'b11: data_out <= dout_16[15:12];
 		endcase
 	end
 
 	always_ff @(posedge clk) begin
-		we_p1 <= we;
-		addr_14 <= addr[15:2];  // discard the lowest two bits of address
-		case (addr[1:0])  // use lowest two bits for nibble selection
-			2'b00: din_16 <= {12'b0,data_in};
-			2'b01: din_16 <= {8'b0,data_in,4'b0};
-			2'b10: din_16 <= {4'b0,data_in,8'b0};
-			2'b11: din_16 <= {data_in,12'b0};
+		case (addr[1:0])  // use lowest two bits for write selection
+			2'b00: begin
+				din_16 <= {12'b0,data_in};
+				nibble <= 4'b0001;
+			end
+			2'b01: begin
+				din_16 <= {8'b0,data_in,4'b0};
+				nibble <= 4'b0010;
+			end
+			2'b10: begin
+				din_16 <= {4'b0,data_in,8'b0};
+				nibble <= 4'b0100;
+			end
+			2'b11: begin
+				din_16 <= {data_in,12'b0};
+				nibble <= 4'b1000;
+			end
 		endcase
-	end
-
-	always_ff @(posedge clk) begin
-		case (addr[1:0])
-			2'b00: nibble <= 4'b0001;
-			2'b01: nibble <= 4'b0010;
-			2'b10: nibble <= 4'b0100;
-			2'b11: nibble <= 4'b1000;
-		endcase
-		nibble_p1 <= nibble;
 	end
 
 	SB_SPRAM256KA spram_inst (
