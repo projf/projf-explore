f334e4a1f20cc5af4b1e59fc1668fdb4afbc8676
Will Green <WillGreen@users.noreply.github.com>
2022-04-01 12:33:32 +0100

    Spring 2022 Minor Improvements (#100)
    * Add Simple Dual-Port BRAM that works on XC7 and iCE40.
    * Improve Vivado test benches for new display modules.
    * Use -O3 by default. Provides large performance uplift on Linux.
    * Improve READMEs. Link to YosysHQ builds. Homebrew installs Xcode CLI.

---
+++ lib/memory/bram_sdp.sv
@@ -0,0 +1,41 @@
+// Project F Library - Simple Dual-Port Block RAM
+// (C)2022 Will Green, Open source hardware released under the MIT License
+// Learn more at https://projectf.io
+
+`default_nettype none
+`timescale 1ns / 1ps
+
+module bram_sdp #(
+    parameter WIDTH=8, 
+    parameter DEPTH=256, 
+    parameter INIT_F="",
+    localparam ADDRW=$clog2(DEPTH)
+    ) (
+    input wire logic clk_write,               // write clock (port a)
+    input wire logic clk_read,                // read clock (port b)
+    input wire logic we,                      // write enable (port a)
+    input wire logic [ADDRW-1:0] addr_write,  // write address (port a)
+    input wire logic [ADDRW-1:0] addr_read,   // read address (port b)
+    input wire logic [WIDTH-1:0] data_in,     // data in (port a)
+    output     logic [WIDTH-1:0] data_out     // data out (port b)
+    );
+
+    logic [WIDTH-1:0] memory [DEPTH];
+
+    initial begin
+        if (INIT_F != 0) begin
+            $display("Loading memory init file '%s' into bram_sdp.", INIT_F);
+            $readmemh(INIT_F, memory);
+        end
+    end
+
+    // Port A: Sync Write
+    always_ff @(posedge clk_write) begin
+        if (we) memory[addr_write] <= data_in;
+    end
+
+    // Port B: Sync Read
+    always_ff @(posedge clk_read) begin
+        data_out <= memory[addr_read];
+    end
+endmodule
