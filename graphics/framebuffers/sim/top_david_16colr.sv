8b3f4716d6fe2965c78582f521330aa9c07effe3
Will Green <WillGreen@users.noreply.github.com>
2022-06-29 12:08:54 +0100

    Add new-style 16-colour David.
    Reverse greyscale to match default grey palette.
    Update mono designs to match 16-colour for painting etc.

--- graphics/framebuffers/sim/top_david_16colr.sv
+++ graphics/framebuffers/sim/top_david_16colr.sv
@@ -1,11 +1,11 @@
-// Project F: Framebuffers - Mono David (Verilator SDL)
+// Project F: Framebuffers - 16 Colour David (Verilator SDL)
 // (C)2022 Will Green, open source hardware released under the MIT License
 // Learn more at https://projectf.io/posts/hardware-sprites/
 
 `default_nettype none
 `timescale 1ns / 1ps
 
-module top_david_mono #(parameter CORDW=16) (  // signed coordinate width (bits)
+module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bits)
     input  wire logic clk_pix,      // pixel clock
     input  wire logic rst_pix,      // sim reset
     output      logic signed [CORDW-1:0] sdl_sx,  // horizontal SDL position
@@ -38,15 +38,19 @@ module top_david_mono #(parameter CORDW=16) (  // signed coordinate width (bits)
 
     // colour parameters
     localparam CHANW = 4;        // colour channel width (bits)
+    localparam COLRW = 3*CHANW;  // colour width: three channels (bits)
+    localparam CIDXW = 4;        // colour index width (bits)
+    localparam PAL_FILE = "../../../lib/res/palettes/grey16_4b.mem";  // palette file
+    // localparam PAL_FILE = "../../../lib/res/palettes/sweetie16_4b.mem";  // palette file
 
     // framebuffer (FB)
     localparam FB_WIDTH  = 160;  // framebuffer width in pixels
     localparam FB_HEIGHT = 120;  // framebuffer width in pixels
     localparam FB_PIXELS = FB_WIDTH * FB_HEIGHT;  // total pixels in buffer
     localparam FB_ADDRW  = $clog2(FB_PIXELS);  // address width
-    localparam FB_DATAW  = 1;  // colour bits per pixel
-    localparam FB_IMAGE  = "../res/david/david_1bit.mem";  // bitmap file
-    // localparam FB_IMAGE  = "../../../lib/res/test/test_box_mono_160x120.mem";  // bitmap file
+    localparam FB_DATAW  = 4;  // colour bits per pixel
+    localparam FB_IMAGE  = "../res/david/david.mem";  // bitmap file
+    // localparam FB_IMAGE  = "../../../lib/res/test/test_box_160x120.mem";  // bitmap file
 
     logic [FB_ADDRW-1:0] fb_addr_read;
     logic [FB_DATAW-1:0] fb_colr_read;
@@ -70,7 +74,7 @@ module top_david_mono #(parameter CORDW=16) (  // signed coordinate width (bits)
     );
 
     // calculate framebuffer read address for display output
-    localparam LAT = 2;  // read_fb+1, BRAM+1
+    localparam LAT = 3;  // read_fb+1, BRAM+1, CLUT+1
     logic read_fb;
     always_ff @(posedge clk_pix) begin
         read_fb <= (sy >= 0 && sy < FB_HEIGHT && sx >= 0-LAT && sx < FB_WIDTH-LAT);
@@ -81,12 +85,28 @@ module top_david_mono #(parameter CORDW=16) (  // signed coordinate width (bits)
         end
     end
 
+    // colour lookup table
+    logic [COLRW-1:0] fb_pix_colr;
+    clut_simple #(
+        .COLRW(COLRW),
+        .CIDXW(CIDXW),
+        .F_PAL(PAL_FILE)
+        ) clut_instance (
+        .clk_write(clk_pix),
+        .clk_read(clk_pix),
+        .we(0),
+        .cidx_write(0),
+        .cidx_read(fb_colr_read),
+        .colr_in(0),
+        .colr_out(fb_pix_colr)
+    );
+
     // paint screen
     logic paint_area;  // area of framebuffer to paint
     logic [CHANW-1:0] paint_r, paint_g, paint_b;  // colour channels
     always_comb begin
          paint_area = (sy >= 0 && sy < FB_HEIGHT && sx >= 0 && sx < FB_WIDTH);
-        {paint_r, paint_g, paint_b} = (paint_area && fb_colr_read) ? 12'hFFF: 12'h000;
+        {paint_r, paint_g, paint_b} = paint_area ? fb_pix_colr: 12'h000;
     end
 
     // SDL output (8 bits per colour channel)
--- graphics/framebuffers/sim/top_david_mono_draw.sv
+++ graphics/framebuffers/sim/top_david_mono_draw.sv
@@ -36,13 +36,17 @@ module top_david_mono_draw #(parameter CORDW=16) (  // signed coordinate width (
         /* verilator lint_on PINCONNECTEMPTY */
     );
 
+    // colour parameters
+    localparam CHANW = 4;        // colour channel width (bits)
+
     // framebuffer (FB)
-    localparam FB_WIDTH  = 160;
-    localparam FB_HEIGHT = 120;
-    localparam FB_PIXELS = FB_WIDTH * FB_HEIGHT;
-    localparam FB_ADDRW  = $clog2(FB_PIXELS);
+    localparam FB_WIDTH  = 160;  // framebuffer width in pixels
+    localparam FB_HEIGHT = 120;  // framebuffer width in pixels
+    localparam FB_PIXELS = FB_WIDTH * FB_HEIGHT;  // total pixels in buffer
+    localparam FB_ADDRW  = $clog2(FB_PIXELS);  // address width
     localparam FB_DATAW  = 1;  // colour bits per pixel
-    localparam FB_IMAGE  = "../res/david/david_1bit.mem";
+    localparam FB_IMAGE  = "../res/david/david_1bit.mem";  // bitmap file
+    // localparam FB_IMAGE  = "../../../lib/res/test/test_box_mono_160x120.mem";  // bitmap file
 
     logic fb_we;
     logic [FB_ADDRW-1:0] fb_addr_write, fb_addr_read;
@@ -112,11 +116,11 @@ module top_david_mono_draw #(parameter CORDW=16) (  // signed coordinate width (
         if (rst_pix) state <= IDLE;
     end
 
-    logic read_fb;  // reading from BRAM takes one cycle: start reading one cycle early
-    always_comb read_fb = (sy >= 0 && sy < FB_HEIGHT && sx >= -1 && sx < FB_WIDTH-1);
-
     // calculate framebuffer read address for display output
+    localparam LAT = 2;  // read_fb+1, BRAM+1
+    logic read_fb;
     always_ff @(posedge clk_pix) begin
+        read_fb <= (sy >= 0 && sy < FB_HEIGHT && sx >= 0-LAT && sx < FB_WIDTH-LAT);
         if (frame) begin  // reset address at start of frame
             fb_addr_read <= 0;
         end else if (read_fb) begin  // increment address in painting area
@@ -125,14 +129,11 @@ module top_david_mono_draw #(parameter CORDW=16) (  // signed coordinate width (
     end
 
     // paint screen
-    logic paint;  
-    always_ff @(posedge clk_pix) paint <= read_fb;  // one cycle for memory read
-    localparam CHANW = 4;  // colour channel width (bits)
-    logic [CHANW-1:0] paint_r, paint_g, paint_b;
+    logic paint_area;  // area of framebuffer to paint
+    logic [CHANW-1:0] paint_r, paint_g, paint_b;  // colour channels
     always_comb begin
-        paint_r = (paint && fb_colr_read) ? 4'hF : 4'h0;
-        paint_g = (paint && fb_colr_read) ? 4'hF : 4'h0;
-        paint_b = (paint && fb_colr_read) ? 4'hF : 4'h0;
+         paint_area = (sy >= 0 && sy < FB_HEIGHT && sx >= 0 && sx < FB_WIDTH);
+        {paint_r, paint_g, paint_b} = (paint_area && fb_colr_read) ? 12'hFFF: 12'h000;
     end
 
     // SDL output (8 bits per colour channel)
