c8d825913ae72a24fad6f0920858bb75dc77f978
Will Green <WillGreen@users.noreply.github.com>
2022-07-01 10:14:00 +0100

    Basic scaling without proper latency correction.

--- graphics/framebuffers/sim/top_david_scale.sv
+++ graphics/framebuffers/sim/top_david_scale.sv
@@ -1,11 +1,11 @@
-// Project F: Framebuffers - 16 Colour David (Verilator SDL)
+// Project F: Framebuffers - Scaled David (Verilator SDL)
 // (C)2022 Will Green, open source hardware released under the MIT License
 // Learn more at https://projectf.io/posts/hardware-sprites/
 
 `default_nettype none
 `timescale 1ns / 1ps
 
-module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bits)
+module top_david_scale #(parameter CORDW=16) (  // signed coordinate width (bits)
     input  wire logic clk_pix,      // pixel clock
     input  wire logic rst_pix,      // sim reset
     output      logic signed [CORDW-1:0] sdl_sx,  // horizontal SDL position
@@ -19,7 +19,7 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
 
     // display sync signals and coordinates
     logic signed [CORDW-1:0] sx, sy;
-    logic de, frame;
+    logic de, frame, line;
     display_480p #(.CORDW(CORDW)) display_inst (
         .clk_pix,
         .rst_pix,
@@ -31,17 +31,18 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
         /* verilator lint_on PINCONNECTEMPTY */
         .de,
         .frame,
-        /* verilator lint_off PINCONNECTEMPTY */
-        .line()
-        /* verilator lint_on PINCONNECTEMPTY */
+        .line
     );
+
+    // screen dimensions (must match display_inst)
+    localparam H_RES      = 640;
 
     // colour parameters
     localparam CHANW = 4;        // colour channel width (bits)
     localparam COLRW = 3*CHANW;  // colour width: three channels (bits)
     localparam CIDXW = 4;        // colour index width (bits)
-    localparam PAL_FILE = "../../../lib/res/palettes/grey16_4b.mem";  // palette file
-    // localparam PAL_FILE = "../../../lib/res/palettes/sweetie16_4b.mem";  // palette file
+    // localparam PAL_FILE = "../../../lib/res/palettes/grey16_4b.mem";  // palette file
+    localparam PAL_FILE = "../../../lib/res/palettes/sweetie16_4b.mem";  // palette file
 
     // framebuffer (FB)
     localparam FB_WIDTH  = 160;  // framebuffer width in pixels
@@ -49,8 +50,8 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
     localparam FB_PIXELS = FB_WIDTH * FB_HEIGHT;  // total pixels in buffer
     localparam FB_ADDRW  = $clog2(FB_PIXELS);  // address width
     localparam FB_DATAW  = CIDXW;  // colour bits per pixel
-    localparam FB_IMAGE  = "../res/david/david.mem";  // bitmap file
-    // localparam FB_IMAGE  = "../../../lib/res/test/test_box_160x120.mem";  // bitmap file
+    // localparam FB_IMAGE  = "../res/david/david.mem";  // bitmap file
+    localparam FB_IMAGE  = "../../../lib/res/test/test_box_160x120.mem";  // bitmap file
 
     logic [FB_ADDRW-1:0] fb_addr_read;
     logic [FB_DATAW-1:0] fb_colr_read;
@@ -73,17 +74,54 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
         .data_out(fb_colr_read)
     );
 
-    // calculate framebuffer read address for display output
-    localparam LAT = 3;  // read_fb+1, BRAM+1, CLUT+1
-    logic read_fb;
+    // calculate framebuffer read address for linebuffer
+    logic [$clog2(FB_WIDTH)-1:0] cnt_lbx;
     always_ff @(posedge clk_pix) begin
-        read_fb <= (sy >= 0 && sy < FB_HEIGHT && sx >= 0-LAT && sx < FB_WIDTH-LAT);
         if (frame) begin  // reset address at start of frame
             fb_addr_read <= 0;
-        end else if (read_fb) begin  // increment address in painting area
+        end else if (sy >= -1 && cnt_lb_rline == 0 && cnt_lbx < FB_WIDTH) begin
             fb_addr_read <= fb_addr_read + 1;
+            cnt_lbx <= cnt_lbx + 1;
+        end
+        if (line) cnt_lbx <= 0;
+    end
+
+    // linebuffer
+    localparam LB_SCALE = 4;
+    logic [$clog2(LB_SCALE):0] cnt_lb_rline;  // count lines for scaling
+    always_ff @(posedge clk_pix) begin
+        if (frame) cnt_lb_rline <= 0;
+        else if (line)  begin
+            cnt_lb_rline <= (cnt_lb_rline == LB_SCALE-1) ? 0 : cnt_lb_rline + 1;
         end
     end
+
+    localparam LAT = 2;  // LB+1, CLUT+1
+    logic paint_area;    // area of screen to paint
+    logic lb_en_out;     // linebuffer enable out
+    always_comb begin
+        lb_en_out  = (sy >= 0 && sy < FB_HEIGHT * LB_SCALE 
+            && sx >= 0-LAT && sx < (FB_WIDTH * LB_SCALE)-LAT);
+        paint_area = (sy >= 0 && sy < FB_HEIGHT * LB_SCALE 
+            && sx >= 0 && sx < (FB_WIDTH * LB_SCALE));
+    end
+
+    // en_out needs to corrected for latency
+    logic [FB_DATAW-1:0] lb_colr_out;
+    linebuffer_simple #(
+        .DATAW(4),
+        .LEN(H_RES),
+        .SCALE(LB_SCALE)
+    ) lb_sf (
+        .clk_in(clk_pix),
+        .clk_out(clk_pix),
+        .rst_in(line),
+        .rst_out(line),
+        .en_in(cnt_lb_rline == 0),
+        .en_out(lb_en_out),
+        .data_in(fb_colr_read),
+        .data_out(lb_colr_out)
+    );
 
     // colour lookup table
     logic [COLRW-1:0] fb_pix_colr;
@@ -96,18 +134,14 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
         .clk_read(clk_pix),
         .we(0),
         .cidx_write(0),
-        .cidx_read(fb_colr_read),
+        .cidx_read(lb_colr_out),
         .colr_in(0),
         .colr_out(fb_pix_colr)
     );
 
     // paint screen
-    logic paint_area;  // area of framebuffer to paint
     logic [CHANW-1:0] paint_r, paint_g, paint_b;  // colour channels
-    always_comb begin
-        paint_area = (sy >= 0 && sy < FB_HEIGHT && sx >= 0 && sx < FB_WIDTH);
-        {paint_r, paint_g, paint_b} = paint_area ? fb_pix_colr: 12'h000;
-    end
+    always_comb {paint_r, paint_g, paint_b} = paint_area ? fb_pix_colr: 12'h000;
 
     // SDL output (8 bits per colour channel)
     always_ff @(posedge clk_pix) begin
