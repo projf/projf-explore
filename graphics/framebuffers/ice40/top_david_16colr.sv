1336bb194a71ab62011bb72f70545a0099ad6b23
Will Green <WillGreen@users.noreply.github.com>
2022-07-17 20:27:15 +0100

    Updated framebuffer for iCE40.
    Not yet tested on hardware.

--- graphics/framebuffers/ice40/top_david_16colr.sv
+++ graphics/framebuffers/ice40/top_david_16colr.sv
@@ -1,34 +1,48 @@
-// Project F: Framebuffers - 16 Colour David (Verilator SDL)
+// Project F: Framebuffers - 16 Colour David (iCEBreaker 12-bit DVI Pmod)
 // (C)2022 Will Green, open source hardware released under the MIT License
 // Learn more at https://projectf.io/posts/framebuffers/
 
 `default_nettype none
 `timescale 1ns / 1ps
 
-module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bits)
-    input  wire logic clk_pix,      // pixel clock
-    input  wire logic rst_pix,      // sim reset
-    output      logic signed [CORDW-1:0] sdl_sx,  // horizontal SDL position
-    output      logic signed [CORDW-1:0] sdl_sy,  // vertical SDL position
-    output      logic sdl_de,       // data enable (low in blanking interval)
-    output      logic sdl_frame,    // high at start of frame
-    output      logic [7:0] sdl_r,  // 8-bit red
-    output      logic [7:0] sdl_g,  // 8-bit green
-    output      logic [7:0] sdl_b   // 8-bit blue
+module top_david_16colr (
+    input  wire logic clk_12m,      // 12 MHz clock
+    input  wire logic btn_rst,      // reset button
+    output      logic dvi_clk,      // DVI pixel clock
+    output      logic dvi_hsync,    // DVI horizontal sync
+    output      logic dvi_vsync,    // DVI vertical sync
+    output      logic dvi_de,       // DVI data enable
+    output      logic [3:0] dvi_r,  // 4-bit DVI red
+    output      logic [3:0] dvi_g,  // 4-bit DVI green
+    output      logic [3:0] dvi_b   // 4-bit DVI blue
     );
 
+    // generate pixel clock
+    logic clk_pix;
+    logic clk_pix_locked;
+    clock_480p clock_pix_inst (
+       .clk_12m,
+       .rst(btn_rst),
+       .clk_pix,
+       .clk_pix_locked
+    );
+
+    // reset in pixel clock domain
+    logic rst_pix;
+    always_comb rst_pix = !clk_pix_locked;  // wait for clock lock
+
     // display sync signals and coordinates
+    localparam CORDW = 16;  // signed coordinate width (bits)
     logic signed [CORDW-1:0] sx, sy;
+    logic hsync, vsync;
     logic de, frame;
     display_480p #(.CORDW(CORDW)) display_inst (
         .clk_pix,
         .rst_pix,
         .sx,
         .sy,
-        /* verilator lint_off PINCONNECTEMPTY */
-        .hsync(),
-        .vsync(),
-        /* verilator lint_on PINCONNECTEMPTY */
+        .hsync,
+        .vsync,
         .de,
         .frame,
         /* verilator lint_off PINCONNECTEMPTY */
@@ -111,14 +125,25 @@ module top_david_16colr #(parameter CORDW=16) (  // signed coordinate width (bit
         {paint_r, paint_g, paint_b} = paint_area ? fb_pix_colr: 12'h000;
     end
 
-    // SDL output (8 bits per colour channel)
-    always_ff @(posedge clk_pix) begin
-        sdl_sx <= sx;
-        sdl_sy <= sy;
-        sdl_de <= de;
-        sdl_frame <= frame;
-        sdl_r <= {2{paint_r}};  // double signal width (assumes CHANW=4)
-        sdl_g <= {2{paint_g}};
-        sdl_b <= {2{paint_b}};
-    end
+    // DVI Pmod output
+    SB_IO #(
+        .PIN_TYPE(6'b010100)  // PIN_OUTPUT_REGISTERED
+    ) dvi_signal_io [14:0] (
+        .PACKAGE_PIN({dvi_hsync, dvi_vsync, dvi_de, dvi_r, dvi_g, dvi_b}),
+        .OUTPUT_CLK(clk_pix),
+        .D_OUT_0({hsync, vsync, de, paint_r, paint_g, paint_b}),
+        /* verilator lint_off PINCONNECTEMPTY */
+        .D_OUT_1()
+        /* verilator lint_on PINCONNECTEMPTY */
+    );
+
+    // DVI Pmod clock output: 180° out of phase with other DVI signals
+    SB_IO #(
+        .PIN_TYPE(6'b010000)  // PIN_OUTPUT_DDR
+    ) dvi_clk_io (
+        .PACKAGE_PIN(dvi_clk),
+        .OUTPUT_CLK(clk_pix),
+        .D_OUT_0(1'b0),
+        .D_OUT_1(1'b1)
+    );
 endmodule
