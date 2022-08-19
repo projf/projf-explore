8903c05314659df555e7ce59e8781777a2bbc3cf
Will Green <WillGreen@users.noreply.github.com>
2022-08-19 09:29:33 +0100

    Update clocks modules in Vivado Project files.

--- demos/life-on-screen/xc7/vivado/create_project.tcl
+++ demos/life-on-screen/xc7/vivado/create_project.tcl
@@ -55,7 +55,7 @@ set_property -name "top_auto_set" -value "0" -objects $fs_design_obj
 
 # Design sources (used in simulation)
 set design_sources [list \
-  [file normalize "${lib_dir}/clock/xc7/clock_gen_480p.sv"] \
+  [file normalize "${lib_dir}/clock/xc7/clock_480p.sv"] \
   [file normalize "${lib_dir}/clock/xd.sv"] \
   [file normalize "${lib_dir}/display/display_480p.sv"] \
   [file normalize "${lib_dir}/display/framebuffer_bram.sv"] \
