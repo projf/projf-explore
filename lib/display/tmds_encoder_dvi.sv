a4e9fe57c90a6241b084e06112ecb51dc030f9c8
Will Green <WillGreen@users.noreply.github.com>
2023-05-02 15:50:50 +0100

    Cleaned up TMDS encoder.
    Needs testing on real hardware.

--- lib/display/tmds_encoder_dvi.sv
+++ lib/display/tmds_encoder_dvi.sv
@@ -1,54 +1,56 @@
 // Project F Library - TMDS Encoder for DVI
-// (C)2022 Will Green, Open source hardware released under the MIT License
+// (C)2023 Will Green, Open source hardware released under the MIT License
 // Learn more at https://projectf.io
 
 `default_nettype none
 `timescale 1ns / 1ps
 
-// NB. This design is from my early FPGA days and needs improvement.
-// I plan to rewrite it in 2022.
-
 module tmds_encoder_dvi (
-    input  wire logic clk_pix,
-    input  wire logic rst_pix,
-    input  wire logic [7:0] data_in,    // colour data
-    input  wire logic [1:0] ctrl_in,    // control data
-    input  wire logic de,               // data enable
-    output      logic [9:0] tmds        // encoded TMDS data
+    input   wire logic clk_pix,
+    input   wire logic rst_pix,
+    input   wire logic [7:0] data_in,  // colour data
+    input   wire logic [1:0] ctrl_in,  // control data
+    input   wire logic de,             // data enable
+    output       logic [9:0] tmds      // encoded TMDS data
     );
 
     // select basic encoding based on the ones in the input data
-    // need to be defined as wire or these won't simulate
-    wire logic [3:0] d_ones = {3'b0,data_in[0]} + {3'b0,data_in[1]} + {3'b0,data_in[2]}
-        + {3'b0,data_in[3]} + {3'b0,data_in[4]} + {3'b0,data_in[5]}
-        + {3'b0,data_in[6]} + {3'b0,data_in[7]};
-    wire logic use_xnor = (d_ones > 4'd4) || ((d_ones == 4'd4) && (data_in[0] == 0));
-
+    logic [3:0] data_1s;
+    logic use_xnor;
+    always_comb begin
+        /* verilator lint_off WIDTH */
+        data_1s = data_in[0] + data_in[1] + data_in[2] + data_in[3] + 
+                    data_in[4] + data_in[5] + data_in[6] + data_in[7];
+        /* verilator lint_on WIDTH */
+        use_xnor = (data_1s > 4'd4) || ((data_1s == 4'd4) && (data_in[0] == 0));
+    end
+     
     // encode colour data with xor/xnor
-    /* verilator lint_off UNOPTFLAT */
+    integer i;
     logic [8:0] enc_qm;
-    assign enc_qm[0] = data_in[0];
-    assign enc_qm[1] = (use_xnor) ? (enc_qm[0] ~^ data_in[1]) : (enc_qm[0] ^ data_in[1]);
-    assign enc_qm[2] = (use_xnor) ? (enc_qm[1] ~^ data_in[2]) : (enc_qm[1] ^ data_in[2]);
-    assign enc_qm[3] = (use_xnor) ? (enc_qm[2] ~^ data_in[3]) : (enc_qm[2] ^ data_in[3]);
-    assign enc_qm[4] = (use_xnor) ? (enc_qm[3] ~^ data_in[4]) : (enc_qm[3] ^ data_in[4]);
-    assign enc_qm[5] = (use_xnor) ? (enc_qm[4] ~^ data_in[5]) : (enc_qm[4] ^ data_in[5]);
-    assign enc_qm[6] = (use_xnor) ? (enc_qm[5] ~^ data_in[6]) : (enc_qm[5] ^ data_in[6]);
-    assign enc_qm[7] = (use_xnor) ? (enc_qm[6] ~^ data_in[7]) : (enc_qm[6] ^ data_in[7]);
-    assign enc_qm[8] = (use_xnor) ? 0 : 1;
-    /* verilator lint_on UNOPTFLAT */
+    always_comb begin
+        enc_qm[0] = data_in[0];
+        for (i=0; i<7; i=i+1) begin
+            /* verilator lint_off ALWCOMBORDER */
+            enc_qm[i+1] = (use_xnor) ? (enc_qm[i] ~^ data_in[i+1]) : (enc_qm[i] ^ data_in[i+1]);
+            /* verilator lint_on ALWCOMBORDER */
+        end
+        enc_qm[8] = (use_xnor) ? 0 : 1;
+    end
 
     // disparity in encoded data for DC balancing: needs to cover -8 to +8
-    wire logic signed [4:0] ones = {4'b0,enc_qm[0]} + {4'b0,enc_qm[1]}
-              + {4'b0,enc_qm[2]} + {4'b0,enc_qm[3]} + {4'b0,enc_qm[4]}
-              + {4'b0,enc_qm[5]} + {4'b0,enc_qm[6]} + {4'b0,enc_qm[7]};
-
-    wire logic signed [4:0] zeros = 5'b01000 - ones;
-    wire logic signed [4:0] balance = ones - zeros;
+    logic signed [4:0] ones, zeros, balance;
+    always_comb begin
+        /* verilator lint_off WIDTH */
+        ones = enc_qm[0] + enc_qm[1] + enc_qm[2] + enc_qm[3] + 
+                    enc_qm[4] + enc_qm[5] + enc_qm[6] + enc_qm[7];
+        /* verilator lint_on WIDTH */
+        zeros   = 5'd8 - ones;
+        balance = ones - zeros;
+    end
 
     // record ongoing DC bias
     logic signed [4:0] bias;
-
     always_ff @ (posedge clk_pix) begin
         if (de == 0) begin  // send control data in blanking interval
             case (ctrl_in)  // ctrl sequences (always have 7 transitions)
