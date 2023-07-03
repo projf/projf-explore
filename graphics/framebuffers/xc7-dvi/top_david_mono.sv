dac5ec237f13d87d45870916bfee9899cdf549c1
Will Green <WillGreen@users.noreply.github.com>
2023-07-03 11:45:45 +0100

    Add Nexys Video version of Framebuffers (requires testing).

---
+++ graphics/framebuffers/xc7-dvi/top_david_mono.sv
@@ -0,0 +1,155 @@
+// Project F: Framebuffers - Mono David (Nexys Video)
+// (C)2023 Will Green, open source hardware released under the MIT License
+// Learn more at https://projectf.io/posts/framebuffers/
+
+`default_nettype none
+`timescale 1ns / 1ps
+
+module top_david_mono (
+    input  wire logic clk_100m,       // 100 MHz clock
+    input  wire logic btn_rst_n,      // reset button
+    output      logic hdmi_tx_ch0_p,  // HDMI source channel 0 diff+
+    output      logic hdmi_tx_ch0_n,  // HDMI source channel 0 diff-
+    output      logic hdmi_tx_ch1_p,  // HDMI source channel 1 diff+
+    output      logic hdmi_tx_ch1_n,  // HDMI source channel 1 diff-
+    output      logic hdmi_tx_ch2_p,  // HDMI source channel 2 diff+
+    output      logic hdmi_tx_ch2_n,  // HDMI source channel 2 diff-
+    output      logic hdmi_tx_clk_p,  // HDMI source clock diff+
+    output      logic hdmi_tx_clk_n   // HDMI source clock diff-
+    );
+
+    // generate pixel clock
+    logic clk_pix;
+    logic clk_pix_5x;
+    logic clk_pix_locked;
+    logic rst_pix;
+    clock_720p clock_pix_inst (
+       .clk_100m,
+       .rst(!btn_rst_n),  // reset button is active low
+       .clk_pix,
+       .clk_pix_5x,
+       .clk_pix_locked
+    );
+    always_ff @(posedge clk_pix) rst_pix <= !clk_pix_locked;  // wait for clock lock
+
+    // display sync signals and coordinates
+    localparam CORDW = 16;  // signed coordinate width (bits)
+    logic signed [CORDW-1:0] sx, sy;
+    logic hsync, vsync;
+    logic de, frame;
+    display_720p #(.CORDW(CORDW)) display_inst (
+        .clk_pix,
+        .rst_pix,
+        .sx,
+        .sy,
+        .hsync,
+        .vsync,
+        .de,
+        .frame,
+        /* verilator lint_off PINCONNECTEMPTY */
+        .line()
+        /* verilator lint_on PINCONNECTEMPTY */
+    );
+
+    // colour parameters
+    localparam CHANW = 4;        // colour channel width (bits)
+    localparam COLRW = 3*CHANW;  // colour width: three channels (bits)
+    localparam BG_COLR = 'h137;  // background colour
+
+    // framebuffer (FB)
+    localparam FB_WIDTH  = 160;  // framebuffer width in pixels
+    localparam FB_HEIGHT = 120;  // framebuffer width in pixels
+    localparam FB_PIXELS = FB_WIDTH * FB_HEIGHT;  // total pixels in buffer
+    localparam FB_ADDRW  = $clog2(FB_PIXELS);  // address width
+    localparam FB_DATAW  = 1;  // colour bits per pixel
+    localparam FB_IMAGE  = "david_1bit.mem";  // bitmap file
+    // localparam FB_IMAGE  = "test_box_mono_160x120.mem";  // bitmap file
+
+    // pixel read address and colour
+    logic [FB_ADDRW-1:0] fb_addr_read;
+    logic [FB_DATAW-1:0] fb_colr_read;
+
+    // framebuffer memory
+    bram_sdp #(
+        .WIDTH(FB_DATAW),
+        .DEPTH(FB_PIXELS),
+        .INIT_F(FB_IMAGE)
+    ) bram_inst (
+        .clk_write(clk_pix),
+        .clk_read(clk_pix),
+        /* verilator lint_off PINCONNECTEMPTY */
+        .we(),
+        .addr_write(),
+        /* verilator lint_on PINCONNECTEMPTY */
+        .addr_read(fb_addr_read),
+        /* verilator lint_off PINCONNECTEMPTY */
+        .data_in(),
+        /* verilator lint_on PINCONNECTEMPTY */
+        .data_out(fb_colr_read)
+    );
+
+    // calculate framebuffer read address for display output
+    localparam LAT = 2;  // read_fb+1, BRAM+1
+    logic read_fb;
+    always_ff @(posedge clk_pix) begin
+        read_fb <= (sy >= 0 && sy < FB_HEIGHT && sx >= -LAT && sx < FB_WIDTH-LAT);
+        if (frame) begin  // reset address at start of frame
+            fb_addr_read <= 0;
+        end else if (read_fb) begin  // increment address in painting area
+            fb_addr_read <= fb_addr_read + 1;
+        end
+    end
+
+    // paint screen
+    logic paint_area;  // area of framebuffer to paint
+    logic [CHANW-1:0] paint_r, paint_g, paint_b;  // colour channels
+    always_comb begin
+        paint_area = (sy >= 0 && sy < FB_HEIGHT && sx >= 0 && sx < FB_WIDTH);
+        {paint_r, paint_g, paint_b} = paint_area ? {COLRW{fb_colr_read}} : BG_COLR;
+    end
+
+    // display colour: paint colour but black in blanking interval
+    logic [CHANW-1:0] display_r, display_g, display_b;
+    always_comb {display_r, display_g, display_b} = (de) ? {paint_r, paint_g, paint_b} : 0;
+
+    // DVI signals (8 bits per colour channel)
+    logic [2*CHANW-1:0] dvi_r, dvi_g, dvi_b;
+    logic dvi_hsync, dvi_vsync, dvi_de;
+    always_ff @(posedge clk_pix) begin
+        dvi_hsync <= hsync;
+        dvi_vsync <= vsync;
+        dvi_de    <= de;
+        dvi_r <= {2{display_r}};
+        dvi_g <= {2{display_g}};
+        dvi_b <= {2{display_b}};
+    end
+
+    // TMDS encoding and serialization
+    logic tmds_ch0_serial, tmds_ch1_serial, tmds_ch2_serial, tmds_clk_serial;
+    dvi_generator dvi_out (
+        .clk_pix,
+        .clk_pix_5x,
+        .rst_pix,
+        .de(dvi_de),
+        .data_in_ch0(dvi_b),
+        .data_in_ch1(dvi_g),
+        .data_in_ch2(dvi_r),
+        .ctrl_in_ch0({dvi_vsync, dvi_hsync}),
+        .ctrl_in_ch1(2'b00),
+        .ctrl_in_ch2(2'b00),
+        .tmds_ch0_serial,
+        .tmds_ch1_serial,
+        .tmds_ch2_serial,
+        .tmds_clk_serial
+    );
+
+    // TMDS output pins
+    tmds_out tmds_ch0 (.tmds(tmds_ch0_serial),
+        .pin_p(hdmi_tx_ch0_p), .pin_n(hdmi_tx_ch0_n));
+    tmds_out tmds_ch1 (.tmds(tmds_ch1_serial),
+        .pin_p(hdmi_tx_ch1_p), .pin_n(hdmi_tx_ch1_n));
+    tmds_out tmds_ch2 (.tmds(tmds_ch2_serial),
+        .pin_p(hdmi_tx_ch2_p), .pin_n(hdmi_tx_ch2_n));
+    tmds_out tmds_clk (.tmds(tmds_clk_serial),
+        .pin_p(hdmi_tx_clk_p), .pin_n(hdmi_tx_clk_n));
+endmodule
